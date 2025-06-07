fa1fe16697d01a7530eb23e6ccc61b91ecaffdf1
Suike08 <keiwater0819@gmail.com>
2025-06-07 15:26:33 +0900

    プレイヤーのステート処理

--- project/Assets/Mizuno/Script/SpriteAnim.cs
+++ project/Assets/Mizuno/Script/SpriteAnim.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteAnim : MonoBehaviour
@@ -9,29 +10,29 @@ public class SpriteAnim : MonoBehaviour
     public Sprite[] AttackSprites;
 
     private SpriteRenderer spriteRenderer;
-    private Player player;
+    private PlayerState playerState;
 
     private int currentFrame = 0;
     private float timer = 0f;
     public float frameDelay = 0.2f;
 
-    private Player.State lastState;
+    private PlayerState.State lastState;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        player = GetComponent<Player>(); 
+        playerState = GetComponent<PlayerState>(); 
         //  �������I�u�W�F�N�g�ɂ���O��
         //  Player �� SpriteAnim ��ʃI�u�W�F�N�g�ɕ������ꍇ�� public Player player;
 
-        lastState = player.CurrentState;
+        lastState = playerState.CurrentState;
         SetFirstFrame(lastState);
     }
 
     void Update()
     {
         //�v���C���[�̏��
-        Player.State currentState = player.CurrentState;
+        PlayerState.State currentState = playerState.CurrentState;
 
         //���O�̏�Ԃƈ��Ȃ���
         if (currentState != lastState)
@@ -49,7 +50,7 @@ public class SpriteAnim : MonoBehaviour
         }
     }
 
-    void SetFirstFrame(Player.State state)
+    void SetFirstFrame(PlayerState.State state)
     {
         Sprite[] sprites = GetSpritesForState(state);
         if (sprites.Length > 0)
@@ -58,7 +59,7 @@ public class SpriteAnim : MonoBehaviour
         }
     }
 
-    void AdvanceFrame(Player.State state)
+    void AdvanceFrame(PlayerState.State state)
     {
         Sprite[] sprites = GetSpritesForState(state);
         if (sprites.Length == 0) return;
@@ -72,14 +73,14 @@ public class SpriteAnim : MonoBehaviour
         spriteRenderer.sprite = sprites[currentFrame];
     }
 
-    Sprite[] GetSpritesForState(Player.State state)
+    Sprite[] GetSpritesForState(PlayerState.State state)
     {
         switch (state)
         {
-            case Player.State.Idle: return IdleSprites;
-            case Player.State.Run: return RunSprites;
-            case Player.State.Jump: return JumpSprites;
-            case Player.State.Attack: return AttackSprites;
+            case PlayerState.State.Idle: return IdleSprites;
+            case PlayerState.State.Run: return RunSprites;
+            case PlayerState.State.Jump: return JumpSprites;
+            case PlayerState.State.Attack: return AttackSprites;
             default: return IdleSprites;
         }
     }
